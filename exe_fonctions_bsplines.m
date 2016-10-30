243b863144afd8a055add75dee56668c4bc61c6b
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-10-30 08:40:50 +0100

    1; a) b) c) faits

---
+++ exe_fonctions_bsplines.m
@@ -0,0 +1,13 @@
+vecteur_noeud = [0 1 2 2 3 4];
+m = 2;
+fonctions_bsplines(vecteur_noeud, m)
+
+
+vecteur_noeud = [0 0 0 0 1 2 3 3 3 3];
+m = 2;
+fonctions_bsplines(vecteur_noeud, m)
+
+
+vecteur_noeud = [0 0 0 1 1 1];
+m = 2;
+fonctions_bsplines(vecteur_noeud, m)
